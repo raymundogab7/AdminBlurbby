562906a9f720c2e5b08c2768e3a347d524efcc2b
gabriel <raymundo.gabriel7@gmail.com>
2016-09-15 19:56:39 +0800

    Update error message inmerchant

--- app/Http/Requests/MerchantRequest.php
+++ app/Http/Requests/MerchantRequest.php
@@ -68,7 +68,7 @@ class MerchantRequest extends Request
             'coy_zip.min' => 'Company postal code minimum is 6.',
             'coy_phone.required' => 'The company phone field is required.',
             'coy_phone.min' => 'The contact number field may not be less than 8 characters.',
-            'coy_phone.max' => 'The contact number field may not be less than 8 characters.',
+            'coy_phone.max' => 'The contact number field may not be more than 8 characters.',
             'outlet_add.required' => 'The outlet address field is required.',
             'outlet_zip.required' => 'The outlet postal zip field is required.',
             'outlet_zip.max' => 'Outlet postal code maxlength is 6',
